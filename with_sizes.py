6fa36f04f2788276d3133a9e44cd6b10de81adab
Carlo <31673755+carlox97@users.noreply.github.com>
2021-09-11 21:28:48 +0200

    Update with_sizes.py

--- with_sizes.py
+++ with_sizes.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 def send_mail():
     import smtplib
