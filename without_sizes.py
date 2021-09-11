cf8531b44bdae8c9a60be998977d5eac340d4f58
Carlo <31673755+carlox97@users.noreply.github.com>
2021-09-11 21:27:38 +0200

    Update without_sizes.py

--- without_sizes.py
+++ without_sizes.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 def send_mail():
     import smtplib
@@ -14,8 +12,8 @@ def send_mail():
 
     sent_from = gmail_user
     to = ['MAIL_BEING_NOTIFIED']
-    subject = 'Billy da 2m disponibile'
-    body = 'https://www.decathlon.it/p/bilanciere-bodybuilding-2m-28mm/_/R-p-9291?mc=8289900 \n\n'
+    subject = 'Articolo Disponibile'
+    body = 'https://www.decathlon.it/p/manubrio-bodybuilding-35cm-28mm/_/R-p-7260?mc=1041986 \n\n'
 
     email_text = """From: %s
 To: %s
@@ -36,28 +34,30 @@ Subject: %s
         print ('Email sent!', dt_string)
     except:
         print ('Something went wrong...')
-
+        
 print('running...')
-
-cool_down = 1800  #sec, ovvero mezzora di pausa dopo una mail
-cadenza = 120 #sec, ovvero ogni quanto controllo se ci sono i dischi
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome("C:\web\chromedriver.exe",options=chrome_options)
 
-driver.get("https://www.decathlon.it/p/bilanciere-bodybuilding-2m-28mm/_/R-p-9291?mc=8289900")    #decathlon link
-driver.set_window_size(1296, 1400)
-element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#didomi-notice-agree-button > span")))
-driver.find_element(By.CSS_SELECTOR, "#didomi-notice-agree-button > span").click()
+driver.get("https://www.decathlon.it/p/manubrio-bodybuilding-35cm-28mm/_/R-p-7260?mc=1041986")
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, ".didomi-continue-without-agreeing").click()   # rifiuta cookies
+
+cool_down = 1800  # sec
 
 while True:
 
-    time.sleep(1)
-
-    if driver.find_element(By.ID, "ctaButton").is_displayed():
+    try:
+        #driver.find_element(By.CSS_SELECTOR, ".cta--block").is_displayed()
+        driver.find_element(By.XPATH,"//*[@id='app']/main/article/div[1]/div[5]/section/article/div/button").is_displayed() # div[1]/div[X]: X può variare per diversi articoli. Se non si è sicuri togliere il commento alla riga sopra e commentare questa.
+    except:
+        time.sleep(120)
+    else:
         send_mail()
-        time.sleep(cool_down-cadenza)
+        time.sleep(cool_down)
 
-    time.sleep(cadenza)
     driver.refresh()
+    time.sleep(4)
+
